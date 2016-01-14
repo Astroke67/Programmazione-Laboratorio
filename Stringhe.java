3691bc0788621af668554bfa940070b95144c7ad
Astroke67 <daniele.colombo9@gmail.com>
2016-01-14 22:15:31 +0100

    Update Stringhe.java

--- Stringhe.java
+++ Stringhe.java
@@ -1,22 +1,42 @@
 import java.io.*;
 import java.util.*;
+
 /*
-Nome: Daniele
-Cognome: Colombo
-Data: 14/01/2016
-Testo esercizio: Dopo aver inserito una stringa, visualizzare quante lettere maiuscole, quante minuscole, quanti numeri.
+Dopo aver inserito una stringa, visualizzare quante lettere maiuscole, quante minuscole, quanti numeri.
 */
 
 public class Stringhe{
 	public static void main (String [] args ){
 		String frase;
-		int lMai;
-		int lMin;
-		int num;
+		int lMai = 0, lMin = 0, num = 0, lung = 0;
+
+		int i=0;
+		char carattere;
 
 		Scanner in = new Scanner(System.in);
 
+		//inserire frase nella variabile
+		System.out.println("Inserisci una frase: ");
+		frase = in.nextLine();
+
+		//conto lunghezza frase
+		lung = frase.length();
+
+		while(i<lung){			
+			carattere = frase.charAt(i);
+
+			if(Character.isUpperCase(carattere)){
+				lMai=lMai+1;
+			}else if(Character.isLowerCase(carattere)){
+				lMin=lMin+1;
+			}else if(carattere>47 && carattere<58){
+				num=num+1;
+			}
+
+			i ++;
+		}
+
+		System.out.println("La frase: " + frase + " e' lunga " + lung +"caratteri");
+		System.out.println("Sono presenti: \n" + lMin + "Lettere minuscole\n" + lMai + " lettere maiuscole\n" + num + " numeri." );
 	}
-
-
 }
