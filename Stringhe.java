b853fe7919d5a62109459a63e83e67bcdc1ee867
Astroke67 <daniele.colombo9@gmail.com>
2016-01-14 12:07:41 +0100

    Create Stringhe.java

---
+++ Stringhe.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+/*
+Nome: Daniele
+Cognome: Colombo
+Data: 14/01/2016
+Testo esercizio: Dopo aver inserito una stringa, visualizzare quante lettere maiuscole, quante minuscole, quanti numeri.
+*/
+
+public class Stringhe{
+	public static void main (String [] args ){
+		String frase;
+		int lMai;
+		int lMin;
+		int num;
+
+		Scanner in = new Scanner(System.in);
+
+	}
+
+
+}
