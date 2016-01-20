ea768dd3471206a93a704a9b28853bbd82245393
Shimizu96 <jacopo.alilla@outlook.com>
2016-01-20 12:40:49 +0100

    Update Robot.java

--- Programmazione/Robot/Robot.java
+++ Programmazione/Robot/Robot.java
@@ -1,75 +1,72 @@
 public class Robot{
-
+	
 	private int posX, posY;
-	private int direzione,passi;
+	private int direzione, passi;
 	private boolean stato;
-
+	private String stato2;
+	private String finale;
+	char[] direzioni = {'N', 'E', 'S', 'W'};
+	
 	private final int N = 0;
 	private final int E = 1;
 	private final int S = 2;
 	private final int W = 3;
-
+	
 	public Robot(int x, int y){
 		posX = x;
 		posY = y;
 		stato = false;
 		direzione = N;
 	}
-
 	public Robot(int x, int y, boolean z){
+		posX = x;
+		posY = y;
 		stato = z;
 		stato = true;
-		direzione = N;
+		direzione = N;		
 	}
-
 	public void accendi(){
 		this.stato = true;
 	}
-
 	public void spegni(){
 		this.stato = false;
 	}
-
 	public void avanti(int passi){
-    if(stato){
-      switch(direzione){
-        case 'N':
-          this.posY+=passi;
-          break;
-        case 'S':
-          this.posY-=passi;
-          break;
-        case 'E':
-          this.posX+=passi;
-          break;
-        case 'W':
-          this.posX-=passi;
-          break;
-      }
-    }
+		if(stato){
+			switch(direzione){
+				case N:
+					this.posY += passi;
+					break;
+				case S:
+					this.posY -= passi;
+					break;
+				case E:
+					this.posX += passi;
+					break;
+				case W:
+					this.posX -= passi;
+					break;
+			}
+		}
 	}
-
 	public void aDestra(){
 		if(stato){
 			this.direzione = (direzione + 1)%4;
 		}
 	}
-
 	public void aSinistra(){
 		if(stato){
 			this.direzione = (direzione + 3)%4;
 		}
 	}
-
 	public String toString(){
-    String statop;
-    char[] direzioni={'N','E','S','W'};
-    if(stato){
-      statop="acceso";
-    }else{
-      statop="spento";
-    }
-    String info = "Il Robot è: " + stato +"\n in posizione X: " +posX+" Y: "+posY+ "\n verso: "+ direzioni[direzione];
-    return info;
+		
+		if(stato){
+			stato2 = "ON";
+		}else{
+			stato2 = "OFF";
+		}
+		finale = "This is a Robot(" + stato2 + ") facing " + direzioni[direzione] + " in position (" + posX + "," + posY + ").";
+		return finale;
 	}
 }
