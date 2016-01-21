ce0863970ce882e9512a221ffd972d4879cc0d9b
Astroke67 <daniele.colombo9@gmail.com>
2016-01-21 17:32:17 +0100

    Create Bancomat.java

---
+++ Programmazione/Banca/Bancomat.java
@@ -0,0 +1,42 @@
+class Bancomat{
+  private String nome,cognome,id;
+  private char operazione;
+  private int saldo;
+  private int soldiDepositati;
+  private int soldiRitirati;
+  //COSTRUTTORE
+  public Bancomat(String n,String c,String cod){
+    this.nome=n;
+    this.cognome=c;
+    this.id=cod;
+  }
+
+
+  public String stampa(){
+    String stringa;
+    stringa="\nNome: "+ nome +"\nCognome: "+cognome+"\nCodice cliente: "+id;
+    return stringa;
+  }
+
+  public void getOperazione(char o){
+    this.operazione=o;
+  }
+
+  Banca banca=new Banca(operazione);
+
+  public int visualizzaSaldo(){
+    saldo=banca.visualizzaSaldoBanca();
+    return saldo;
+  }
+
+  public void depositaDenaro(int valore){
+    this.soldiDepositati=valore;
+    banca.depositaDenaroBanca(soldiDepositati);
+  }
+  public void ritiraDenaro(int valore){
+    this.soldiRitirati=valore;
+    banca.ritiraDenaroBanca(soldiRitirati);
+  }
+
+
+}
