095801aa9fb86ec3a4b697ba7940685e420463e2
AGirola <andrea.girola95@gmail.com>
2016-01-25 11:36:54 +0100

    Create Calcoli.java

---
+++ Programmazione/CalcolatriceClassi/Calcoli.java
@@ -0,0 +1,30 @@
+class Calcoli {
+
+	public static float doSomma (float num1, float num2){
+		float somma = 0;
+
+		somma = num1+num2;
+		return somma;
+	}
+
+	public static float doSottrazione (float num1, float num2){
+		float sottrazione = 0;
+
+		sottrazione = num1-num2;
+		return sottrazione;
+	}
+
+	public static float doDivisione (float num1, float num2){
+		float divisione = 0;
+
+		divisione = num1/num2;
+		return divisione;
+	}
+
+	public static float doMoltiplicazione (float num1, float num2){
+		float moltiplicazione = 0;
+
+		moltiplicazione = num1*num2;
+		return moltiplicazione;
+	}
+}
