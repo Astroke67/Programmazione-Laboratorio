40276f2cf5f8781b6f667df79292c2c4aadd5e17
Astroke67 <daniele.colombo9@gmail.com>
2016-01-28 21:33:35 +0100

    Update Banca.java

--- Programmazione/Banca/Banca.java
+++ Programmazione/Banca/Banca.java
@@ -1,22 +1,62 @@
+import java.io.*;
+import java.util.*;
+
 class Banca{
-  private char operazione;
   private int soldi;
-  private int saldo=1000;
+  private int saldo=0;
+  private String no;
+  private String codice;
+  private File file;
+
   //COSTRUTTORE
-  public Banca(){
+  public Banca(String co){
+    this.codice=co;
+
   }
 
-  public int visualizzaSaldoBanca(){
+  public int visualizzaSaldoBanca(File input){
+    String stringa;
+    try {
+      PrintWriter writer = new PrintWriter(input);
+      writer.print("");
+      writer.close();
+      BufferedWriter out = new BufferedWriter(new FileWriter(input, true));
+      stringa=String.valueOf(saldo);
+      out.write(stringa);
+      out.close();
+    } catch (IOException e) {
+      System.out.println("ERRORE!" + e);
+    }
     return saldo;
+
   }
 
   public void  depositaDenaroBanca(int deposito){
     this.soldi=deposito;
+    String stringa=" ";
     saldo+=soldi;
   }
-  public void  ritiraDenaroBanca(int prelievo){
+
+  public void  ritiraDenaroBanca(int prelievo,File file){
+
     this.soldi=prelievo;
-    saldo-=soldi;
+    String stringa=" ";
+    if((saldo-soldi)<0){
+        System.out.println("IMPOSSIBILE COMPLETARE LA RICHIESTA:\nSALDO INSUFFICIENTE!");
+    }else{
+        saldo-=soldi;
+    }
   }
 
+
+  public void leggiFileBanca(File file,Scanner scanner,String nome,String cognome,String id){
+      String stringa=" ";
+      while(scanner.hasNextLine()){
+          stringa=scanner.nextLine();
+          int saldod=Integer.parseInt(stringa);
+          this.saldo=saldod;
+      }
+
+
+  }
 }
