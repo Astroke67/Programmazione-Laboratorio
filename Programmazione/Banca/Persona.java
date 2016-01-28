38c97e2d073e42a95488df64e0e06a952fbb3a1d
Astroke67 <daniele.colombo9@gmail.com>
2016-01-28 21:34:27 +0100

    Update Persona.java

--- Programmazione/Banca/Persona.java
+++ Programmazione/Banca/Persona.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
 
-class Personat{
-  public static void main(String[] args){
-    String nome,cognome,id;
+class Persona{
+  public static void main(String[] args) throws IOException{
+    String nome,cognome,id,codseg;
     char scelta=' ';
     int soldi;
     Scanner in = new Scanner (System.in);
+
+
 
     System.out.println("Inserisci Nome: ");
     nome= in.nextLine();
@@ -14,8 +16,17 @@ class Personat{
     cognome= in.nextLine();
     System.out.println("Inserisci Codice Cliente: ");
     id= in.nextLine();
+    System.out.println("Inserisci Codice Segreto (6 cifre): ");
+    codseg= in.nextLine();
+
+    File input = new File(id+codseg+".txt");
+    Scanner inputFile = new Scanner(input);
+
 
     Bancomat b= new Bancomat(nome,cognome,id);
+
+    b.leggiFile(input,inputFile,nome,cognome,id);
+
 
     //System.out.println(b.stampa());
     while(scelta!='4'){
@@ -30,27 +41,28 @@ class Personat{
       switch(scelta){
         case '1':
           //Visualizzare saldo.
-          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo()+" euro");
+          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo(input)+" euro");
           break;
         case '2':
           //Depositare denaro
           System.out.println("Quanto vuoi depositare?");
           soldi=in.nextInt();
           b.depositaDenaro(soldi);
-          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo()+" euro");
+          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo(input)+" euro");
 
           break;
         case '3':
           //Ritirare denaro
           System.out.println("Quanto vuoi ritirare?");
           soldi=in.nextInt();
-          b.ritiraDenaro(soldi);
-          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo()+" euro");
+          b.ritiraDenaro(soldi,input);
+          System.out.println("\n\nSaldo attuale: " + b.visualizzaSaldo(input)+" euro");
           break;
         case '4':
           System.out.println(b.stampa());
-          System.out.println("\n\nSaldo finale: " + b.visualizzaSaldo()+" euro");
+          System.out.println("\n\nSaldo finale: " + b.visualizzaSaldo(input)+" euro");
           System.out.println("Arrivederci!");
+
           break;
         default:
           System.out.println("SCELTA NON VALIDA!");
