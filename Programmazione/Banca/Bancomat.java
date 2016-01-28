2bf56deb9bf86ea513e3f1bc7ac091de8d588806
Astroke67 <daniele.colombo9@gmail.com>
2016-01-28 21:33:58 +0100

    Update Bancomat.java

--- Programmazione/Banca/Bancomat.java
+++ Programmazione/Banca/Bancomat.java
@@ -1,9 +1,12 @@
+import java.io.*;
+import java.util.*;
+
 class Bancomat{
   private String nome,cognome,id;
-  private char operazione;
   private int saldo;
   private int soldiDepositati;
   private int soldiRitirati;
+  private File file;
   //COSTRUTTORE
   public Bancomat(String n,String c,String cod){
     this.nome=n;
@@ -11,16 +14,19 @@ class Bancomat{
     this.id=cod;
   }
 
+
   public String stampa(){
     String stringa;
     stringa="\nNome: "+ nome +"\nCognome: "+cognome+"\nCodice cliente: "+id;
     return stringa;
   }
 
-  Banca banca=new Banca();
 
-  public int visualizzaSaldo(){
-    saldo=banca.visualizzaSaldoBanca();
+  Banca banca=new Banca(id);
+
+  public int visualizzaSaldo(File filein){
+    this.file=filein;
+    saldo=banca.visualizzaSaldoBanca(file);
     return saldo;
   }
 
@@ -28,8 +34,14 @@ class Bancomat{
     this.soldiDepositati=valore;
     banca.depositaDenaroBanca(soldiDepositati);
   }
-  public void ritiraDenaro(int valore){
+  public void ritiraDenaro(int valore,File file){
     this.soldiRitirati=valore;
-    banca.ritiraDenaroBanca(soldiRitirati);
+    banca.ritiraDenaroBanca(soldiRitirati,file);
   }
+
+  public void leggiFile(File file,Scanner scanner,String nome, String cognome,String id){
+    banca.leggiFileBanca(file,scanner,nome,cognome,id);
+  }
+
+
 }
